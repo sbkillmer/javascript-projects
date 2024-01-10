896f7c9883466dc25d483313635899ff9df907c2
sbkillmer <sethk1600@gmail.com>
2024-01-10 03:41:12 -0600

    Ch5 Exercise: Declare and Initialize

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,6 +1,14 @@
 // Declare and initialize the variables for exercise 1 here:
 
+let engineIndicatorLight = 'red blinking';
+let spaceSuitsOn = true;
+let shutteCabinReady = true;
+let crewStatus = spaceSuitsOn && shutteCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+// I predict "engines are off" will print as the first two conditions aren't met
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
@@ -9,3 +17,5 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+// I predicted correctly
